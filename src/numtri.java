c4949c1425000481782964ed2687a8e6c0575b7c
ermanno <moser.ermanno@gmail.com>
2016-12-15 23:32:27 +0100

    numtri solution. I had some problems with this one: at first I tried to build a tree to search through, and I had a problem (maybe very slow, possibly a cycle), anyway I realized that I didn't really need to build up an actual tree in order to solve the problem, so I didn't investigate the issue further, though this might mean that I should spend some time ironing out some things and make sure that I can code such simple implementations *fast*, it's a sport, a run against time.

--- src/numtri.java
+++ src/numtri.java
@@ -7,21 +7,14 @@ TASK: numtri
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
-class NumtriNode {
-    NumtriNode left, right;
+class Tree {
     int value;
-    int largestSum;
-
-    public NumtriNode(int value) {
-        this.value = value;
-    }
+    int sum;
 
     @Override
     public String toString() {
-        return String.valueOf(this.value);
+        return String.valueOf(value);
     }
 }
 
@@ -32,73 +25,65 @@ class numtri {
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("numtri.out")));
 
         int numOfRows = Integer.parseInt(f.readLine());
-
-        NumtriNode root = buildTree(f, numOfRows);
-
-        int largestSum = searchSolution(root);
-
-        out.println(largestSum);
+        int solution = calculateSolution(f, numOfRows);
         
+        out.println(solution);
+
         out.close();
     }
 
-    private static int searchSolution(NumtriNode root) {
-        Queue<NumtriNode> nodes = new LinkedBlockingQueue<NumtriNode>();
-        int largestSum = 0;
-        root.largestSum = root.value;
-        nodes.add(root);
-        while (!nodes.isEmpty()) {
-            NumtriNode n = nodes.remove();
-            if (n.left == null && n.right == null) {
-                // if we are a leaf we update the "global" largest sum
-                largestSum = Math.max(largestSum, n.largestSum);
-            } else {
-                // if we are not a leaf, we update the largest sum of our children
-                n.left.largestSum = Math.max(n.left.largestSum, n.left.value + n.largestSum);
-                n.right.largestSum = Math.max(n.right.largestSum, n.right.value + n.largestSum);
-                nodes.add(n.left);
-                nodes.add(n.right);
-            }
-        }
-        return largestSum;
-    }
+    private static int calculateSolution(BufferedReader f, int numOfRows) throws IOException {
+        int globalSum = 0;
+        
+        // Create the "root" of the "tree"
+        int number = Integer.parseInt(f.readLine());
+        Tree t = new Tree();
+        t.value = number;
+        t.sum = t.value;
+        
+        // Add the root to the queue
+        Queue<Tree> queue = new LinkedList<Tree>();
+        
+        queue.add(t);
 
-    private static NumtriNode buildTree(BufferedReader f, int numOfRows) throws IOException {
-        Queue<NumtriNode> nodes = new LinkedBlockingQueue<NumtriNode>();
-        NumtriNode root = new NumtriNode(Integer.parseInt(f.readLine()));
-        nodes.add(root);
-        NumtriNode right = null;
-        for (int row = 2; row <= numOfRows; row++) {
+        for (int i = 2; i <= numOfRows; i++) {
             StringTokenizer st = new StringTokenizer(f.readLine());
-            while (st.hasMoreTokens()) {
-                NumtriNode parent = nodes.remove();
-                if (right != null) {
-                    parent.left = right;
-                } else {
-                    parent.left = new NumtriNode(Integer.parseInt(st.nextToken()));
-                    nodes.add(parent.left);
-                }
-                parent.right = right = new NumtriNode(Integer.parseInt(st.nextToken()));
-                nodes.add(parent.right);
-            }
-            right = null;
-        }
-        return root;
-    }
 
-    /**
-     * Prints the "tree" rooted at *root* in a breath-first mode.
-     * @param root
-     */
-    private static void printTree(NumtriNode root) {
-        Queue<NumtriNode> nodes = new LinkedBlockingQueue<NumtriNode>();
-        nodes.add(root);
-        while (!nodes.isEmpty()) {
-            NumtriNode n = nodes.remove();
-            System.out.println(n.value);
-            if (n.left != null) nodes.add(n.left);
-            if (n.right != null) nodes.add(n.right);
+            // As the first element in the i-th row, it is the left child of the first node of the "previous row"
+            Tree parent = queue.remove();
+            t = new Tree();
+            t.value = Integer.parseInt(st.nextToken());
+            t.sum = parent.sum + t.value;
+            
+            queue.add(t);
+
+            // We then look ad the other nodes, barring the last: they are the right child of the current parent,
+            // and the left child of the next parent, the sibling of the current parent
+            for (int j = 2; j <= i - 1; j++) {
+                t = new Tree();
+                t.value = Integer.parseInt(st.nextToken());
+                t.sum = parent.sum + t.value;
+
+                parent = queue.remove();
+                t.sum = Math.max(t.sum, parent.sum + t.value);
+                
+                queue.add(t);
+            }
+            
+            // The last node is the right child of the last node in the previous row
+            t = new Tree();
+            t.value = Integer.parseInt(st.nextToken());
+            t.sum = parent.sum + t.value;
+            
+            queue.add(t);
         }
+
+        // We have left in the queue the leaves, the max is one of their sums
+        for (Tree tr : queue) {
+            globalSum = Math.max(tr.sum, globalSum);
+        }
+
+        return globalSum;
     }
 
 }
