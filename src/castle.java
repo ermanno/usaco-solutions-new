02033c4656298c4ee3c81b8e2b275c81f98d6762
ermanno <moser.ermanno@gmail.com>
2017-02-03 21:57:23 +0100

    start of chapter 2

---
+++ src/castle.java
@@ -0,0 +1,20 @@
+
+/*
+ID: moser.el
+LANG: JAVA
+TASK: castle
+ */
+
+import java.io.*;
+
+class castle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("castle.in"));
+        PrintWriter out = new PrintWriter(new FileWriter("castle.out"));
+
+        
+        
+        f.close();
+        out.close();
+    }
+}
