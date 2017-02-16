e523b292e0662a41f135c531a0edbcb2a7e447ca
ermanno <moser.ermanno@gmail.com>
2017-02-16 23:13:49 +0100

    min swaps when sorting three values list

--- src/sort3.java
+++ src/sort3.java
@@ -8,7 +8,65 @@ import java.io.*;
 import java.util.*;
 
 class sort3 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("sort3.in"));
+        PrintWriter out = new PrintWriter(new FileWriter("sort3.out"));
+
+        int N = Integer.valueOf(f.readLine());
+        int[] numbers = new int[N];
+        int ones = 0;
+        int twos = 0;
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(f.readLine());
+            if (numbers[i] == 1)
+                ones++;
+            if (numbers[i] == 2)
+                twos++;
+        }
+
+        int numOfSwaps = minSwaps(numbers, ones, twos);
+
+        out.println(numOfSwaps);
+
+        f.close();
+        out.close();
+    }
+
+    public static int minSwaps(int[] numbers, int ones, int twos) {
+        int N = numbers.length;
+        int numOfSwaps = 0;
+        for (int i = ones; i < N; i++) {
+            // ones ... twos ... threes, we cycle over "twos ... threes"
+            // looking for ones
+            if (numbers[i] == 1) {
+                // we need to swap it, thus we look to the "ones" and search for "twos"
+                int posToSwap = 0;
+                for (int j = 0; j < ones; j++) {
+                    if (numbers[j] == 2) {
+                        posToSwap = j;
+                        break;
+                    } else if (numbers[j] == 3) {
+                        posToSwap = j;
+                    }
+                }
+                swap(numbers, i, posToSwap);
+                numOfSwaps++;
+            }
+        }
         
+        // we have already places the ones
+        for (int i = ones + twos; i < N; i++) {
+            if (numbers[i] == 2) {
+                numOfSwaps++;
+            }
+        }
+        return numOfSwaps;
+    }
+
+    public static void swap(int[] numbers, int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
     }
 }
--- test/sort3Test.java
+++ test/sort3Test.java
@@ -1,3 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class sort3Test {
+
+    @Test
+    public void testCalcNumOfSwaps() {
+        int[] numbers = { 1, 1, 3, 2, 1, 1, 1, 3, 2, 1, 3, 3, 2, 1, 3, 1, 1, 2, 3, 1 };
+        int ones = 10;
+        int twos = 4;
+
+        int minSwaps = sort3.minSwaps(numbers, ones, twos);
+        assertEquals(6, minSwaps);
+    }
 
 }
