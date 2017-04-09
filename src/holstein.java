53d2e7ea90c4afbb140ae7808eef0b2c805175e2
ermanno <moser.ermanno@gmail.com>
2017-04-09 17:12:37 +0200

    Finished holstein exercise

--- src/holstein.java
+++ src/holstein.java
@@ -9,22 +9,23 @@ import java.io.*;
 import java.util.*;
 
 class holstein {
-    private static int numVitamins;
-    private static int numFeeds;
-    private static int[] vitamins;
-    private static int[][] feeds;
+
+    static int numVitamins;
+    static int vitaminRequirements[];
+    static int numFeeds;
+    static int feeds[][];
+    static List<Integer> tempSolution = new ArrayList<Integer>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("holstein.in"));
         PrintWriter out = new PrintWriter(new FileWriter("holstein.out"));
 
         numVitamins = Integer.parseInt(f.readLine());
-        vitamins = new int[numVitamins];
+        vitaminRequirements = new int[numVitamins];
         StringTokenizer st = new StringTokenizer(f.readLine());
         for (int i = 0; i < numVitamins; i++) {
-            vitamins[i] = Integer.parseInt(st.nextToken());
+            vitaminRequirements[i] = Integer.parseInt(st.nextToken());
         }
-        
         numFeeds = Integer.parseInt(f.readLine());
         feeds = new int[numFeeds][numVitamins];
         for (int i = 0; i < numFeeds; i++) {
@@ -33,26 +34,76 @@ class holstein {
                 feeds[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        
-        
+
+        searchForSolution(new ArrayList<Integer>(), 0);
+
+        out.print(tempSolution.size());
+        for (int elem : tempSolution) {
+            out.print(" " + (elem + 1));
+        }
+        out.println();
+
         f.close();
         out.close();
     }
-    
-    public static boolean isBetter(boolean[] newSolution, int costNewSolution, boolean[] oldSolution, int costOldSolution) {
-        int solutionLength = newSolution.length;
 
-        if (costNewSolution < costOldSolution) return true;
-        if (costNewSolution > costOldSolution) return false;
-        
-        for (int i = 0; i < solutionLength; i++) {
-            if (newSolution[i] && !oldSolution[i]) {
-                return true;
-            } else if (!newSolution[i] && oldSolution[i]) {
-                return false;
+    public static void searchForSolution(List<Integer> solution, int pos) {
+        if (pos == numFeeds) {
+            Collections.sort(solution);
+            if (isSolution(solution) && isBetter(solution, tempSolution)) {
+                tempSolution = copy(solution);
             }
+        } else {
+            // take
+            solution.add(pos);
+            searchForSolution(solution, pos + 1);
+
+            // don't take
+            solution.remove(solution.size() - 1);
+            searchForSolution(solution, pos + 1);
         }
-        return false; // they are the same solution, we don't change
+    }
+
+   
+    
+    private static List<Integer> copy(List<Integer> solution) {
+        List<Integer> copy = new ArrayList<Integer>(solution.size());
+        copy.addAll(solution);
+        return copy;
+    }
+
+    private static boolean isBetter(List<Integer> newSolution, List<Integer> oldSolution) {
+        if (oldSolution.isEmpty())
+            return true;
+        if (newSolution.size() < oldSolution.size())
+            return true;
+        if (newSolution.size() > oldSolution.size())
+            return false;
+        for (int i = 0; i < newSolution.size(); i++) {
+            if (newSolution.get(i) < oldSolution.get(i))
+                return true;
+            if (newSolution.get(i) > oldSolution.get(i))
+                return false;
+        }
+        return false; // if we exit here, the solutions are the same, therefore
+                      // the new one is not strictly "better"
+    }
+
+    private static boolean isSolution(List<Integer> solution) {
+        int[] vitamins = new int[numVitamins];
+        for (Integer element : solution) {
+            addToSolution(vitamins, feeds[element]);
+        }
+        for (int i = 0; i < numVitamins; i++) {
+            if (vitaminRequirements[i] > vitamins[i])
+                return false;
+        }
+        return true;
+    }
+
+    private static void addToSolution(int[] vitamins, int[] feed) {
+        for (int i = 0; i < numVitamins; i++) {
+            vitamins[i] += feed[i];
+        }
     }
 }
